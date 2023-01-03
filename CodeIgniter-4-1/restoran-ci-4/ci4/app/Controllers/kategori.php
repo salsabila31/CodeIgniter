765e39f8c063fa973c68ad34544579daf9603305
salsabila31 <110610642+salsabila31@users.noreply.github.com>
2023-01-03 22:43:47 +0700

    Codeigniter 4-1

---
+++ CodeIgniter-4-1/restoran-ci-4/ci4/app/Controllers/kategori.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+class kategori extends BaseController
+{
+    public function index()
+    {
+        //return view('welcome_message');
+        echo "<h1>Belajar ci4</h1>";
+    }
+}
