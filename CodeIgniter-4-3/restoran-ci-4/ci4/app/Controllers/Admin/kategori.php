1ff46d56ae64a79dad63d1761b953baa49c987f3
salsabila31 <110610642+salsabila31@users.noreply.github.com>
2023-01-03 22:49:50 +0700

    Codeigniter 4-3

---
+++ CodeIgniter-4-3/restoran-ci-4/ci4/app/Controllers/Admin/kategori.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+use App\Controllers\BaseController;
+
+class kategori extends BaseController
+{
+    public function index()
+    {
+        //return view('welcome_message');
+        echo "<h1>Belajar ci4</h1>";
+    }
+    public function select()
+    {
+        echo "menampilkan semua data";
+    }
+    public function selectwhere($id = null)
+    {
+        echo "menampilkan data yang dipilih";
+    }
+    public function formInsert()
+    {
+        echo "menampilkan form insert";
+    }
+    public function formUpdate()
+    {
+        echo "menampilkan form update";
+    }
+    public function update($id = null)
+    {
+        echo "proses update data $id";
+    }
+    public function delete($id = null)
+    {
+        echo "proses delete data";
+    }
+}
