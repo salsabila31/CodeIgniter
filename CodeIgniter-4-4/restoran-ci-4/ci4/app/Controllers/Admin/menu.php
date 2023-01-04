d7c7b4ca9fbae6b61bb064ba04268b1af5cd54b4
salsabila31 <110610642+salsabila31@users.noreply.github.com>
2023-01-04 08:59:11 +0700

    Codeigniter 4-4

---
+++ CodeIgniter-4-4/restoran-ci-4/ci4/app/Controllers/Admin/menu.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controllers;
+
+class menu extends BaseController
+{
+    public function index()
+    {
+        return view('welcome_message');
+    }
+    public function select()
+    {
+        echo "<h1>untuk menampilkan data</h1> ";
+    }
+    public function update($id = null, $nama = null)
+    {
+        echo "<h1>untuk update data dengan id : $id $nama</h1>";
+    }
+}
